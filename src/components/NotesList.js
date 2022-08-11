babf163867e192011825216c687f1c92e45ee6f7
Godspower Idiakose <idiakeg@gmail.com>
2022-08-11 02:11:39 +0100

    Imported and used context in the component and formated the component render the amount of notes present in the notes state. Also imported and rendered the AddNote component

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Note from "./Note";
+import { useContext } from "react";
+import Context from "../contexts/Context";
+import AddNote from "./AddNote";
 
 const NotesList = () => {
+	let { notes } = useContext(Context);
 	return (
 		<div className="note-list">
-			<Note />
-			<Note />
-			<Note />
-			<Note />
+			{notes.map((note) => (
+				<Note text={note.text} id={note.id} date={note.date} key={note.id} />
+			))}
+			<AddNote />
 		</div>
 	);
 };
