e810946662054080eddb8dff7e2045d99d276880
Godspower Idiakose <idiakeg@gmail.com>
2022-08-11 23:08:19 +0100

    imported and used context in the AddNote component. I also made the text area 'controlled' by giving it a value and onChnage attribute

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useContext } from "react";
+import Context from "../contexts/Context";
 
 const AddNote = () => {
+	const { noteText, handleChange, handleSave } = useContext(Context);
 	return (
 		<div className="note new-note">
 			<textarea
 				cols="10"
 				rows="8"
 				placeholder="Type to add a note..."
+				value={noteText}
+				onChange={handleChange}
 			></textarea>
 			<div className="note-footer">
 				<small>200 Remaining</small>
-				<button className="save">Save</button>
+				<button className="save" onClick={() => handleSave(noteText)}>
+					Save
+				</button>
 			</div>
 		</div>
 	);
