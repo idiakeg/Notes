28d09e94086bcbc6b41d0fe72ceec21b4e6db00f
Godspower Idiakose <idiakeg@gmail.com>
2022-08-11 23:06:48 +0100

    Created the 'handleSave' and 'handleChange' event handlers, and the noteText state

--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -4,6 +4,7 @@ import { nanoid } from "nanoid";
 const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
+	// USE STATE DEFINITIONS
 	const [notes, setNotes] = useState([
 		{
 			text: "This is my first note!!",
@@ -21,8 +22,43 @@ export const ContextProvider = ({ children }) => {
 			id: nanoid(),
 		},
 	]);
+
+	const [noteText, setNoteText] = useState("");
+
+	// EVENT HANDLER DEFINITIONS
+	const handleChange = (e) => {
+		const newTextNote = e.target.value;
+		setNoteText(newTextNote);
+	};
+
+	const handleSave = (text) => {
+		if (text.trim().length > 0) {
+			let noteObj = {
+				text,
+				date: "12/06/22",
+				id: nanoid(),
+			};
+			setNotes((current) => {
+				return [...current, noteObj];
+			});
+
+			setNoteText("");
+		}
+	};
+
 	return (
-		<Context.Provider value={{ notes, setNotes }}>{children}</Context.Provider>
+		<Context.Provider
+			value={{
+				notes,
+				setNotes,
+				noteText,
+				setNoteText,
+				handleChange,
+				handleSave,
+			}}
+		>
+			{children}
+		</Context.Provider>
 	);
 };
 
